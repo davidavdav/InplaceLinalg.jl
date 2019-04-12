b3543eb2973a05db1784cdcc9811c092f9aeb030
David A. van Leeuwen <david.vanleeuwen@gmail.com>
2019-04-12 19:40:54 +0200

    Add support for transposition of A and/or B in @inplace C = A * B

--- src/InplaceLinalg.jl
+++ src/InplaceLinalg.jl
@@ -11,9 +11,14 @@ end
 function inplace(e::Expr)
     ## dump(e)
     if e.head == :(=) && isa(e.args[1], Symbol) && isa(e.args[2], Expr) && e.args[2].head == :call && e.args[2].args[1] == :*
-        return :(BLAS.gemm!('N', 'N', 1.0, $(e.args[2].args[2]), $(e.args[2].args[3]), 0.0, $(e.args[1])))
+        tr1, arg1 = trans(e.args[2].args[2])
+        tr2, arg2 = trans(e.args[2].args[3])
+        return :(BLAS.gemm!($tr1, $tr2, 1.0, $arg1, $arg2, 0.0, $(e.args[1])))
     end
     return e
 end
+
+trans(expr::Expr) = (expr.head == Symbol("'")) ? ('T', expr.args[1]) : ('N', expr)
+trans(x) = 'N', x
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,11 +1,23 @@
 using InplaceLinalg
 using Test
 
-A = randn(10, 20)
-B = randn(20, 10)
+A = randn(100, 200)
+B = randn(200, 100)
 
-C = zeros(10, 10)
+C = zeros(100, 100)
 C1 = A * B
 @time @inplace C = A * B
 
 @test C1 == C
+
+Bt = collect(B')
+At = collect(A')
+
+@time @inplace C = At' * B
+@test C == C1
+
+@time @inplace C = A * Bt'
+@test C == C1
+
+@time @inplace C = At' * Bt'
+@test C == C1
