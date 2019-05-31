2e92fb009570244152d20cf05bce1320a1b926da
bsxfan <bsxfan@gmail.com>
2019-05-31 20:50:30 +0200

    added support for triangular LHS in multiplicative updates

--- src/extend_ldiv_and_rdiv.jl
+++ src/extend_ldiv_and_rdiv.jl
@@ -5,3 +5,18 @@ rdiv!(B::BlasArray, α::Number) = rmul!(B, 1 /α )
 
 ldiv!(U::UniformScaling, B::BlasArray) = lmul!(1 / U.λ, B)
 rdiv!(B::BlasArray, U::UniformScaling) = rmul!(B, 1 / U.λ)
+
+
+
+for (LoTr,UpTr) in ( (LowerTriangular, UpperTriangular) , (UpperTriangular, LowerTriangular) )
+    for (tra,Tra) in ( (transpose,Transpose), (adjoint,Adjoint) )
+        @eval begin
+            ldiv!(L::$LoTr{T}, R::$Tra{T,<:$UpTr{T}}) where T = 
+                $tra(rdiv!($tra(R),$tra(L)))
+            #
+            rdiv!(L::$Tra{T,<:$LoTr{T}}, R::$UpTr{T}) where T = 
+                $tra(ldiv!($tra(R),$tra(L)))
+            #
+        end
+    end
+end
